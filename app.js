ec114c51b6feba8fb46b23c46ad668fba4468495
SergeVernez <sergevernez.pro@gmail.com>
2024-12-03 16:28:13 +0100

    end

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-	const toto = document.querySelector(".scrollTop");
+	const btn = document.querySelector(".scrollTop");
 
 	// affiche la flèche au scroll a 200px
 	window.addEventListener("scroll", () => {
 		if (window.scrollY > 200) {
 			//.scrollY (y maj) foncton liée au js
-			toto.classList.add("visible"); //.classList fonction liée au js
+			btn.classList.add("visible"); //.classList fonction liée au js
 		} else {
-			toto.classList.remove("visible");
+			btn.classList.remove("visible");
 		}
 	});
 
 	// retour en haut de page
-	toto.addEventListener("click", () => {
+	btn.addEventListener("click", () => {
 		window.scrollTo({
 			top: 0,
 			left: 0,
