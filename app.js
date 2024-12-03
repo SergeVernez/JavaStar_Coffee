3b4da08ca235be37b79b55ddc8376b5fc18d72e6
SergeVernez <sergevernez.pro@gmail.com>
2024-12-03 11:31:13 +0100

    fix modif scrollTop

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-const toto = document.querySelector(".scrollTop");
+document.addEventListener("DOMContentLoaded", () => {
+	const toto = document.querySelector(".scrollTop");
 
-toto.addEventListener('click', () => {
-	
-	window.scrollTo({
-		top: 0,
-		left: 0,
-		behavior: "smooth",
+	// affiche la flèche au scroll a 200px
+	window.addEventListener("scroll", () => {
+		if (window.scrollY > 200) {
+			//.scrollY (y maj) foncton liée au js
+			toto.classList.add("visible"); //.classList fonction liée au js
+		} else {
+			toto.classList.remove("visible");
+		}
+	});
+
+	// retour en haut de page
+	toto.addEventListener("click", () => {
+		window.scrollTo({
+			top: 0,
+			left: 0,
+			behavior: "smooth",
+		});
 	});
 });
